b022fbd4e9b42e97e400e348e4b72e45e2b562ba
marcelo <marcelo.devopseng@gmail.com>
2021-11-13 17:32:40 -0300

    eslint

--- myapp/index.js
+++ myapp/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/',(req, res)=> {
-    res.send('Hello World')
+app.get('/', (req, res) =>{
+    res.send("TESTANDO!!")
 })
+
 
 app.listen(port, () =>{
     console.log('Servidor rodando na porta 3000')
